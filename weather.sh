b21d151ae8b365765f94bf2a3650e35812e09483
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2018-11-16 01:16:23 +0100

    Added jq functionality

--- weather.sh
+++ weather.sh
@@ -2,4 +2,5 @@
 base_url='http://api.apixu.com/v1/current.json?key=bef4b1337a6c4781826215259181511'
 location='Poznan'
 full_url=$base_url+'&q='+$location
-curl $full_url
+weatherJson="$(curl $full_url | jq '.')"
+echo "${weatherJson}"
