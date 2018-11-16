0577f4baeb41d6961d6926806e7aaa7d967a2da5
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2018-11-16 02:02:50 +0100

    Slightly extended functionality

--- weather.sh
+++ weather.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-temp=1
+loc=1
 location="Poznan"
 while getopts l:f o
 do	case "$o" in
 	l)	location="$OPTARG";;
-	f)	temp=2;;
+	f)	loc=2;;
 	esac
 done
 
@@ -13,12 +13,15 @@ full_url=$base_url+'&q='+$location
 weatherJson="$(curl $full_url | jq '.')"
 locationData="$(echo $weatherJson | jq '.location.name')"
 weatherData="$(echo $weatherJson | jq '.current.condition.text')"
-if [ $temp -eq 1 ]
+if [ $loc -eq 1 ]
 then
   tempData="$(echo $weatherJson | jq '.current.temp_c')"
+  windData="$(echo $weatherJson | jq '.current.wind_kph')""kph"
 else
   tempData="$(echo $weatherJson | jq '.current.temp_f')"
+  windData="$(echo $weatherJson | jq '.current.wind_mph')""mph"
 fi
 echo "Showing weather for ${locationData}"
 echo "Weather today is ${weatherData}"
 echo "Temperature ${tempData}"
+echo "Wind blows ${windData}"
