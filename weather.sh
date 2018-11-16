5306fd63c5ea830e6e9b063646a4aa853852970c
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2018-11-16 01:34:31 +0100

    GETOPS implementation

--- weather.sh
+++ weather.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+while getopts ":l:p:" o; do
+    case "${o}" in
+        l)
+            location=${OPTARG}
+            ;;
+        h)
+            p=${OPTARG}
+            ;;
+        *)
+            ;;
+    esac
+done
 base_url='http://api.apixu.com/v1/current.json?key=bef4b1337a6c4781826215259181511'
-location='Poznan'
 full_url=$base_url+'&q='+$location
 weatherJson="$(curl $full_url | jq '.')"
-echo "${weatherJson}"
+locationdata="$(echo $weatherJson | jq '.location.name')"
+echo "Showing weather for ${locationdata}"
