d668b903ee57152809eda8b41d3f9c6c77c0802a
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2018-11-16 12:38:15 +0100

    Location check

--- weather.sh
+++ weather.sh
@@ -12,16 +12,21 @@ base_url='http://api.apixu.com/v1/current.json?key=bef4b1337a6c47818262152591815
 full_url=$base_url+'&q='+$location
 weatherJson="$(curl $full_url | jq '.')"
 locationData="$(echo $weatherJson | jq '.location.name')"
-weatherData="$(echo $weatherJson | jq '.current.condition.text')"
-if [ $loc -eq 1 ]
+if [[ "$locationData" = "null" ]]
 then
-  tempData="$(echo $weatherJson | jq '.current.temp_c')"
-  windData="$(echo $weatherJson | jq '.current.wind_kph')""kph"
+  echo "Unknown location"
 else
-  tempData="$(echo $weatherJson | jq '.current.temp_f')"
-  windData="$(echo $weatherJson | jq '.current.wind_mph')""mph"
+  weatherData="$(echo $weatherJson | jq '.current.condition.text')"
+  if [ $loc -eq 1 ]
+  then
+    tempData="$(echo $weatherJson | jq '.current.temp_c')"
+    windData="$(echo $weatherJson | jq '.current.wind_kph')""kph"
+  else
+    tempData="$(echo $weatherJson | jq '.current.temp_f')"
+    windData="$(echo $weatherJson | jq '.current.wind_mph')""mph"
+  fi
+  echo "Showing weather for ${locationData}"
+  echo "Weather today is ${weatherData}"
+  echo "Temperature ${tempData}"
+  echo "Wind blows ${windData}"
 fi
-echo "Showing weather for ${locationData}"
-echo "Weather today is ${weatherData}"
-echo "Temperature ${tempData}"
-echo "Wind blows ${windData}"
