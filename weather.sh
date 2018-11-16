576b0c5d194b5f60220765818cdd218f9105d574
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2018-11-16 01:55:01 +0100

    Basic functionality

--- weather.sh
+++ weather.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-while getopts ":l:p:" o; do
-    case "${o}" in
-        l)
-            location=${OPTARG}
-            ;;
-        h)
-            p=${OPTARG}
-            ;;
-        *)
-            ;;
-    esac
+temp=1
+location="Poznan"
+while getopts l:f o
+do	case "$o" in
+	l)	location="$OPTARG";;
+	f)	temp=2;;
+	esac
 done
+
 base_url='http://api.apixu.com/v1/current.json?key=bef4b1337a6c4781826215259181511'
 full_url=$base_url+'&q='+$location
 weatherJson="$(curl $full_url | jq '.')"
-locationdata="$(echo $weatherJson | jq '.location.name')"
-echo "Showing weather for ${locationdata}"
+locationData="$(echo $weatherJson | jq '.location.name')"
+weatherData="$(echo $weatherJson | jq '.current.condition.text')"
+if [ $temp -eq 1 ]
+then
+  tempData="$(echo $weatherJson | jq '.current.temp_c')"
+else
+  tempData="$(echo $weatherJson | jq '.current.temp_f')"
+fi
+echo "Showing weather for ${locationData}"
+echo "Weather today is ${weatherData}"
+echo "Temperature ${tempData}"
