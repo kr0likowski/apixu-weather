5793c878d4a55e450e07159ebad04431c5b0e656
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2018-11-19 15:45:15 +0100

    Cache and ascii-art

--- weather.sh
+++ weather.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 loc=1
 update=0
-time=300
+updatetime=300
+tmp="./tmp/weather.json"
 location="Poznan"
 key=${APIXUKEY:-'http://api.apixu.com/v1/current.json?key=bef4b1337a6c4781826215259181511'}
 while getopts ":l:fu" o
@@ -11,22 +12,49 @@ do	case "${o}" in
   u)  update=1;;
 	esac
 done
+location="$(echo "\"$location\"" | awk '{print toupper($1)}')"
+function updateData()
+{
+	curl --create-dirs -o $tmp $full_url
+}
 function getData()
 {
-  weatherJson="$(curl $full_url | jq '.')"
+	if [ -f $tmp ]
+	then
+		currentDate="$(date +%s)"
+		tmpModTime="$(date -r $tmp +%s)"
+		modTime=$((currentDate-tmpModTime))
+		tmpLoc="$(cat $tmp | jq '.location.name')"
+		tmpLoc="$(echo $tmpLoc | awk '{print toupper($1)}')"
+		if [[ "$location" != "$tmpLoc" ]]
+		then
+			modTime=$(($updatetime+1))
+		fi
+	else
+			modTime=$(($updatetime+1))
+	fi
+	if [[ $modTime -gt $updatetime ]]
+	then
+		updateData
+	fi
+}
+function displayData()
+{
+  weatherJson="$(cat $tmp | jq '.')"
   locationData="$(echo $weatherJson | jq '.location.name')"
   if [[ "$locationData" = "null" ]]
   then
     echo "Unknown location"
   else
     clear
+		ascii_art
     weatherData="$(echo $weatherJson | jq '.current.condition.text')"
     if [ $loc -eq 1 ]
     then
-      tempData="$(echo $weatherJson | jq '.current.temp_c')"
+      tempData="$(echo $weatherJson | jq '.current.temp_c')C"
       windData="$(echo $weatherJson | jq '.current.wind_kph')""kph"
     else
-      tempData="$(echo $weatherJson | jq '.current.temp_f')"
+      tempData="$(echo $weatherJson | jq '.current.temp_f')F"
       windData="$(echo $weatherJson | jq '.current.wind_mph')""mph"
     fi
 		echo "=============================="
@@ -35,11 +63,22 @@ function getData()
     echo "Temperature ${tempData}"
     echo "Wind blows ${windData}"
 		echo "=============================="
+		a=$((updatetime/60))
+		echo "Updating every $a minutes"
   fi
   if [[ $1 > 0 ]]
   then
     sleep $1
   fi
+}
+function ascii_art(){
+echo "██╗    ██╗███████╗ █████╗ ████████╗██╗  ██╗███████╗██████╗     ";
+echo "██║    ██║██╔════╝██╔══██╗╚══██╔══╝██║  ██║██╔════╝██╔══██╗    ";
+echo "██║ █╗ ██║█████╗  ███████║   ██║   ███████║█████╗  ██████╔╝    ";
+echo "██║███╗██║██╔══╝  ██╔══██║   ██║   ██╔══██║██╔══╝  ██╔══██╗    ";
+echo "╚███╔███╔╝███████╗██║  ██║   ██║   ██║  ██║███████╗██║  ██║    ";
+echo " ╚══╝╚══╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝    ";
+echo "                                                               ";
 }
 	base_url=$key
 	full_url=$base_url+'&q='+$location
@@ -47,8 +86,10 @@ if [ $update -eq 1 ]
 then
   while [ $update -eq 1 ]
   do
-    getData $time
+		getData
+    displayData $updatetime
   done
 else
-  getData
+	getData
+  displayData
 fi
